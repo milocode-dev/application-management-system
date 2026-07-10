bccdb879c3047582d3abc1def51946919e156ea5
milocode-dev <radityaakmal277@gmail.com>
2026-07-10 23:00:05 +0700

    (Before this commit is adding seeder) this commit description is adding route and controller with --resource and make 7 route simply and automatically

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Projects Page</title>
+</head>
+<body>
+    <h1>Welcome to Projects Page!</h1>
+
+    <ul>
+        @foreach ($projects as $project)
+            <li>{{ $project->user->name }} - {{ $project->title }} - {{ $project->description }}</li>
+        @endforeach
+    </ul>
+</body>
+</html>
